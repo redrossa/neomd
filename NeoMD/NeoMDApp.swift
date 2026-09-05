4b19b65a0031a0d70da34ef1f585b534f0ac7250
Reno Raksi <adriano.raksi@gmail.com>
2026-09-05 02:01:39 -0500

    Initial Commit

---
+++ NeoMD/NeoMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoMDApp.swift
+//  NeoMD
+//
+//  Created by Reno Raksi on 9/5/26.
+//
+
+import SwiftUI
+
+@main
+struct NeoMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeoMDTests/NeoMDTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeoMDTests.swift
+//  NeoMDTests
+//
+//  Created by Reno Raksi on 9/5/26.
+//
+
+import Testing
+@testable import NeoMD
+
+struct NeoMDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
